43b2d88109ab7bb4a4db679e074ea921a42103ff
djmapet <djmapet@gmail.com>
2021-05-14 19:24:23 +0900

    carsでviewsとurlsを追加

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from .models import Car
-import csv
-from io import
+from cars.models import Car
+from django.http import HttpResponse
+
 # Create your views here.
+def index(request):
+    return HttpResponse("index")
+
+def detail(request, car_id=1):
+    return HttpResponse("car id = %d" % car_id)
