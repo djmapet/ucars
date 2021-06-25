066aa665fbbe7e915bb25f482197f77fb6488820
djmapet <djmapet@gmail.com>
2021-06-25 11:29:02 +0900

    一旦古いフォームを削除してmodelformのみにする

--- cars/views/search.py
+++ cars/views/search.py
@@ -9,35 +9,19 @@ def top(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            #maker_id = form.cleaned_data['manufacturer']
             selected_carmodel = form.cleaned_data['carmodel']
-            color_id = form.cleaned_data['color']
-            gear_id = form.cleaned_data['gear']
+            selected_color = form.cleaned_data['color']
+            selected_gear = form.cleaned_data['gear']
             mileage = form.cleaned_data['mileage']
 
             try:
-                #models = CarModel.objects.filter(pk=carmodel_id)
                 carmodel_result = Car.objects.filter(carmodel=int(selected_carmodel.id))
             except Manufacturer.DoesNotExist:
                 raise Http404("maker does not exist")
     else:
         form = SearchForm()
 
-    try:
-        makers = Manufacturer.objects.all()
-    except Manufacturer.DoesNotExist:
-        raise Http404("maker does not exist")
-
-    try:
-        car_models = CarModel.objects.all()
-    except CarModel.DoesNotExist:
-        raise Http404("car model does not exist")
-
     context = {
-        'makers': makers,
-        'car_models': car_models,
-        'gears' : Car.GEAR_CHOICES,
-        'color' : Car.COLOR_CHOICES,
         'carmodel_result' : carmodel_result,
         'form' : form,
     }
