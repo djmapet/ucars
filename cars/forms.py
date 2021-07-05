343af863a8d76a3ea05c6b62b5a3132e0d85d261
djmapet <djmapet@gmail.com>
2021-07-05 17:06:14 +0900

    Revert "Revert "Merge pull request #16 from djmapet/feature/search""
    This reverts commit cd3a7b09bea76ccfc97a2a8a01dccae472f2e041.

---
+++ cars/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from cars.models import Car, Manufacturer, CarModel
+
+class SearchForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = {'carmodel', 'body_type','gear', 'color', 'mileage','latest_inspection_date','price'}
+
+    def clean_carmodel(self):
+        carmodel = self.cleaned_data.get('carmodel')
+        return carmodel
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['carmodel'].required = False
+        self.fields['latest_inspection_date'].required = False
+        self.fields['mileage'].required = False
+        self.fields['price'].required = False
---
+++ cars/views/search.py
@@ -0,0 +1,55 @@
+from cars.models import Car, Manufacturer, CarModel
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404,render
+from cars.forms import SearchForm
+
+
+def top(request):
+    cars = None
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            selected_carmodel = form.cleaned_data['carmodel']
+            selected_body_type = form.cleaned_data['body_type']
+            selected_color = form.cleaned_data['color']
+            selected_gear = form.cleaned_data['gear']
+            mileage = form.cleaned_data['mileage']
+            price = form.cleaned_data['price']
+            latest_inspection_date = form.cleaned_data['latest_inspection_date']
+
+            try:
+                cars = Car.objects.all()
+                if selected_carmodel:
+                    cars = cars.filter(carmodel=int(selected_carmodel.id))
+                if selected_body_type:
+                    cars = cars.filter(body_type=selected_body_type)
+                if selected_color:
+                    cars = cars.filter(color=selected_color)
+                if selected_gear:
+                    cars = cars.filter(gear=selected_gear)
+                if mileage != None:
+                    cars = cars.filter(mileage__lt=mileage)
+                if latest_inspection_date:
+                    cars = cars.filter(latest_inspection_date__lt=latest_inspection_date)
+                if price != None:
+                    cars = cars.filter(price__lt=price)
+
+
+            except Manufacturer.DoesNotExist:
+                raise Http404("maker does not exist")
+    else:
+        form = SearchForm()
+
+    context = {
+        'carmodel_result' : cars,
+        'form' : form,
+    }
+
+
+    return render(request, 'search.html', context)
+
+
+
+def results(request):
+    car = get_object_or_404(Car, pk=car_id)
+    return render(request, 'results.html', {'car': car})
