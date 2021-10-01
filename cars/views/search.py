11bb79e2a60d6f36cf04a291a3c3117a45a00441
Ai Tsujikawa <tuji.kamome@gmail.com>
2021-10-01 15:57:26 +0900

    search.pyのselected_gearを修正(2回目)

--- cars/views/search.py
+++ cars/views/search.py
@@ -26,7 +26,7 @@ def top(request):
                     cars = cars.filter(body_type=selected_body_type)
                 if selected_color:
                     cars = cars.filter(color__in=selected_color)
-                if selected_gear != int(selected_gear):
+                if int(selected_gear) != -1:
                     cars = cars.filter(gear=selected_gear)
                     print("selected_gear = %s" % selected_gear)
                 if mileage != None:
