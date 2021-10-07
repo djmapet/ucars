f66a4ebd0cb56c50f918184fdd3839fe670bc9d0
Ai Tsujikawa <tuji.kamome@gmail.com>
2021-10-07 19:20:55 +0900

    topページの変更とtopページに別リンクを試しに追加

--- cars/urls.py
+++ cars/urls.py
@@ -3,12 +3,12 @@ from .views import car, shop , search
 
 urlpatterns = [
     path('car/<int:car_id>', car.detail),
-    path('maker/<int:manufacturer_id>', car.maker_cars),
+    path('maker/<int:manufacturer_id>', car.maker_cars,name='maker'),
     path('shop/cars/<int:shop_id>', shop.shop_cars),
     path('shop/<int:shop_id>', shop.shop_info),
     path('edit/<int:car_id>', car.edit, name="edit"),
     path('edit/', car.edit, name="edit_new"),
-    path('search', search.top, name='search'),
+    path('', search.top, name='search'),
     path('results/<int:car_id>', search.results, name='result'),
-    path('', car.maker_list),
+    path('maker_list', car.maker_list),
 ]
--- cars/views/car.py
+++ cars/views/car.py
@@ -120,4 +120,3 @@ def searchform(request):
 
 
 
-
