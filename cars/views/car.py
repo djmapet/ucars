b5401acf2f35903ecf4303d35f9020619d3b269a
djmapet <djmapet@gmail.com>
2021-12-02 11:34:05 +0900

    ファイルのアップロードとサンプルで表示を追加

--- cars/views/car.py
+++ cars/views/car.py
@@ -131,13 +131,17 @@ def upload_file(request):
         form = UploadFileForm(request.POST,request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file']) #おかしい
-            return HttpResponse('success/url/')
+            #return HttpResponse('success/url/')
+            context = {
+                'img_path': '/media/images/sample.jpg',
+            }
+            return render(request,'uploaded.html', context)
     else:
         form = UploadFileForm()
     return render(request, 'upload.html',{'form':form})
 
 def handle_uploaded_file(f):
-    with open('sample.jpg', 'wb+') as destination: #合わない
+    with open('media/images/sample.jpg', 'wb+') as destination: #合わない
         for chunk in f.chunks():
             destination.write(chunk)
 
--- ucars/settings.py
+++ ucars/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cars',
     'debug_toolbar',
+    'media',
 ]
 
 MIDDLEWARE = [
@@ -147,5 +147,5 @@ DEBUG_TOOLBAR_PANELS = [
     'debug_toolbar.panels.profiling.ProfilingPanel',
 ]
 
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
