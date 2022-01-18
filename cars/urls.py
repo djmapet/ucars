87f9be4aa7560501fab348b431628d5d34b119f0
Ai Tsujikawa <tuji.kamome@gmail.com>
2022-01-18 15:01:47 +0900

    urls.pyを編集とURLの修正

--- cars/urls.py
+++ cars/urls.py
@@ -7,22 +7,24 @@ from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('car/<int:car_id>', car.detail), #画像のアップロードを追加したい
+    path('', search.top, name='search'),  # トップページ
+#管理者用
+    path('site_admin/', admin.site.urls),
+    path('edit/<int:car_id>', car.edit, name="edit_car"),
+    path('edit_new_car/', car.edit, name="edit_new_car"),  # 新規登録
+    path('car/<int:car_id>', car.detail),  # 画像のアップロードを追加したい
+    path('upload_car_pic/<int:car_id>', car.upload_file, name='upload'),
+    path('upload_car_pic/file/name.txt', car.upload_file, name='upload_file'),
+    path('success/url/', car.upload_file, name='success_file'),
+#閲覧者用
     path('maker/<int:manufacturer_id>', car.maker_cars,name='maker'),
     path('shop/cars/<int:shop_id>', shop.shop_cars),
     path('shop/<int:shop_id>', shop.shop_info),
-    path('edit/<int:car_id>', car.edit, name="edit"),
-    path('edit/', car.edit, name="edit_new"),
-    path('', search.top, name='search'),
     path('results/<int:car_id>', search.results, name='result'),
     path('maker_list', car.maker_list),
     path('shop_list',shop.shop_list),
     path('shop_info/<int:shop_id>',shop.shop_info),
     path('my_page/',car.mypage, name='my_page'),
-    path('upload/<int:car_id>',car.upload_file,name='upload'),
-    path('upload/file/name.txt',car.upload_file,name='upload_file'),
-    path('success/url/',car.upload_file,name='success_file'),
-    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
